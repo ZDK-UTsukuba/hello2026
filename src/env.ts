87e6c32ea887062b19a89d83a1420da17a166510
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2026-03-06 17:38:37 +0900

    add: logs

--- src/env.ts
+++ src/env.ts
@@ -5,29 +5,30 @@ const esaEndpoint: string | undefined = import.meta.env.ESA_ENDPOINT;
 const esaToken: string | undefined = import.meta.env.ESA_TOKEN;
 
 if (
-  isNaN(esaFaqNumber) ||
-  !Number.isInteger(esaFaqNumber) ||
-  esaFaqNumber <= 0
+    isNaN(esaFaqNumber) ||
+    !Number.isInteger(esaFaqNumber) ||
+    esaFaqNumber <= 0
 ) {
-  throw new Error("ESA_FAQ_NUMBER must be an integer");
+    console.error("Invalid ESA_FAQ_NUMBER:", import.meta.env.ESA_FAQ_NUMBER);
+    throw new Error("ESA_FAQ_NUMBER must be an integer");
 }
 
 if (!esaMock) {
-  if (!esaPostCategory) {
-    throw new Error("ESA_POST_CATEGORY is not defined");
-  }
-  if (!esaEndpoint) {
-    throw new Error("ESA_ENDPOINT is not defined");
-  }
-  if (!esaToken) {
-    throw new Error("ESA_TOKEN is not defined");
-  }
+    if (!esaPostCategory) {
+        throw new Error("ESA_POST_CATEGORY is not defined");
+    }
+    if (!esaEndpoint) {
+        throw new Error("ESA_ENDPOINT is not defined");
+    }
+    if (!esaToken) {
+        throw new Error("ESA_TOKEN is not defined");
+    }
 }
 
 export const esa = {
-  faqNumber: esaFaqNumber,
-  mock: esaMock,
-  endpoint: esaEndpoint ?? "",
-  postCategory: esaPostCategory ?? "",
-  token: esaToken ?? "",
+    faqNumber: esaFaqNumber,
+    mock: esaMock,
+    endpoint: esaEndpoint ?? "",
+    postCategory: esaPostCategory ?? "",
+    token: esaToken ?? "",
 };
