393fe82d907a06c858a2bb5ec9a64264ab4f3c29
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2026-03-11 00:26:27 +0900

    nofollowを追加

--- src/esa-utils/external-link-replacer.ts
+++ src/esa-utils/external-link-replacer.ts
@@ -6,14 +6,23 @@ function isExternalLink(href: string): boolean {
   /**
    * @see https://docs.astro.build/ja/guides/environment-variables/
    */
-  if (href.startsWith(import.meta.env.SITE)) {
+  if (href.startsWith(import.meta.env.SITE) || href.startsWith("/")) {
     return false;
   }
   return true;
 }
 
+function isZDKLink(href: string): boolean {
+  const prefixes = [
+    "https://www.zdk.tsukuba.ac.jp/",
+    "https://www.stb.tsukuba.ac.jp/~zdk/",
+  ];
+  return prefixes.some((prefix) => href.startsWith(prefix));
+}
+
 export function externalLinkReplacer() {
   const externalLinkNodes: AnchorNode[] = [];
+  const noZdkLinkNodes: AnchorNode[] = [];
   return async (tree: Node) => {
     visit(tree, (node) => {
       if (isAnchor(node)) {
@@ -21,12 +30,19 @@ export function externalLinkReplacer() {
         if (isExternalLink(properties.href)) {
           externalLinkNodes.push(node);
         }
+        if (!isZDKLink(properties.href)) {
+          noZdkLinkNodes.push(node);
+        }
       }
     });
 
     for (const node of externalLinkNodes) {
       const { properties } = node;
       properties.target = "_blank";
+    }
+    for (const node of noZdkLinkNodes) {
+      const { properties } = node;
+      properties.rel = "noopener nofollow";
     }
   };
 }
